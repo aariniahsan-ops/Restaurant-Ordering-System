d86633a62bc9e4e52b429cf9cc619bab10181a13
aariniahsan-ops <aarini.ahsan@gmail.com>
2026-09-03 19:27:56 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+total = 0
+st.title("Restaurant")
+prices={"Pizza🍕":8,"Pasta🍝":7,"Burger🍔":6,"Sandwich🥪":5}
+name=st.text_input("What is the name of the order")
+phone=st.text_input("Please enter your phone number")
+food=st.radio("Choose your favorite food",["Pizza🍕","Pasta🍝","Burger🍔","Sandwich🥪"])
+quantity=st.number_input("Enter the amount of items")
+pay=st.radio("Cash or Card?",["Cash💵","Card💳"])
+st.write(quantity)
+fries=st.checkbox("Fries")
+cheese=st.checkbox("Cheese")
+gb=st.checkbox("Garlic Bread")
+ice_c=st.checkbox("Ice Cream")
+if st.button("Submit Order"):
+    st.write(food)
+    st.write("Select the toppings")
+    if cheese:
+        st.write("Cheese🧀-1$")
+        total=total+1
+    if fries:
+        st.write("Fries🍟-3$")
+        total=total+3
+    if gb:
+        st.write("Garlic Bread🧄🥖-2$")
+        total=total+2
+    if ice_c:
+        st.write("Ice Cream🍨-4$")
+        total=total+4
+    st.write(name)
+    st.write(phone)
+    st.write("Items Ordered")
+    item_total = quantity * prices[food]
+    total = total + item_total
+    st.subheader(f"The total bill will be ${total}💵")
+    st.write("Payment Method=" , pay , ".")
+
+
